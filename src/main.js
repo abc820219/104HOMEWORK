dac8cc63b5b997a83f428e51cadd87018008a003
z27089433@gmail.com <z27089433@gmail.com>
2021-10-30 21:55:50 +0800

    優化

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import '@/assets/scss/all.scss'
 Vue.config.productionTip = false
 new Vue({
+  mounted() {
+    document.dispatchEvent(new Event('render-event'));
+  },
   render: h => h(App),
 }).$mount('#app')
